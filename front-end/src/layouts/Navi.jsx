22a6429a1dcb792a0b00dac0a0277bef3a8e8af5
Zeynep Baydemir <zeynepbydemir@gmail.com>
2021-07-19 19:52:47 +0300

    SignedIn and SignedOut added. Navi and App.css edited.

--- front-end/src/layouts/Navi.jsx
+++ front-end/src/layouts/Navi.jsx
@@ -1,35 +1,40 @@
-import React from 'react'
-import { Button, Container, Menu } from 'semantic-ui-react'
+import React, { useState } from 'react'
+import { Container, Menu } from 'semantic-ui-react'
+import SignedOut from './SignedOut'
+import SignedIn from "./SignedIn";
 
 export default function Navi() {
+    const [isAuthenticated, setIsAuthenticated] = useState(true)
+
+    function handleSignOut() {
+        setIsAuthenticated(false)
+    }
+
+    function handleSignIn() {
+        setIsAuthenticated(true)
+    }
+
+
     return (
         <div>
-            
-            <div style={{ paddingBottom: "10px",background: "linear-gradient(130deg,#ff7a18,#af002d 41.07%,#319197 76.05%)" }}></div>
 
-            <Menu >
+            <div style={{ paddingBottom: "10px", background: "linear-gradient(130deg,#ff7a18,#af002d 41.07%,#319197 76.05%)" }}></div>
+            <a>
+            <Menu className="Navi">
                 <Container>
-                    <Menu.Item 
-                    name='home'
-                />
-                <Menu.Item
-                    name='messages'
+                    <Menu.Item
+                        name='home'
+                    />
+                    <Menu.Item
+                        name='messages'
+                    />
 
-                />
-                
-                <Menu.Menu position='right'>
-                    
-
-                    <Menu.Item>
-                        <Button primary >Kayıt Ol</Button>
-                    </Menu.Item>
-                    <Menu.Item>
-                        <Button>Giriş Yap</Button>
-                    </Menu.Item>
-                </Menu.Menu>
+                    <Menu.Menu position='right'>
+                        {isAuthenticated ? <SignedIn signOut={handleSignOut} bisey="1" /> : <SignedOut signIn={handleSignIn} />}
+                    </Menu.Menu>
                 </Container>
-                
             </Menu>
+            </a> 
         </div>
     )
 }
---
+++ front-end/src/layouts/SignedOut.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Button } from 'semantic-ui-react'
+
+export default function SignedOut({signIn}) {
+    return (
+        <div className="SignedOut">
+            
+            <Button onClick={signIn}>Giriş Yap</Button>
+            <Button primary style={{marginLeft:'0.5em'}}>Kayıt Ol</Button>
+            
+        </div>
+    )
+}
